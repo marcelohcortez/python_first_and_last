1ccffa1dfcb1bd741b8317bb416fe6a03152e0e3
Marcelo Cortez <marcelohcortez@gmail.com>
2022-02-05 20:30:27 +0000

    create main file, create find_start function

---
+++ first_and_last.py
@@ -0,0 +1,29 @@
+def find_start(arr, target):
+    left, right = 0, len(arr) - 1
+
+    if arr[0] == target:
+        return 0
+    
+    while left <= right:
+        mid = ( left + right ) // 2
+        if arr[mid] == target and arr[mid-1] < target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+    
+def find_end(arr, target):
+    pass
+
+def find_first_and_last(arr, target):
+    start = find_start(arr, target)
+    end = find_end(arr, target)
+    print(f'It starts in the index {start} and ends in the index {end}\n')
+
+if __name__ == '__main__':
+    target = 6
+    arr = [1,3,5,5,5,6,6,6,6,6,6,6,7,9,16,20,26,26,50,89]
+
+    find_first_and_last(arr, target)
