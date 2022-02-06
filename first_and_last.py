7b799441ed58bcc40774d956020e05c3d5daf721
Marcelo Cortez <marcelohcortez@gmail.com>
2022-02-06 08:50:08 +0000

    Add find_end function and verification on main

--- first_and_last.py
+++ first_and_last.py
@@ -15,11 +15,28 @@ def find_start(arr, target):
     return -1
     
 def find_end(arr, target):
-    pass
+    left, right = 0, len(arr) - 1
+
+    if arr[-1] == target:
+        return len(arr) - 1
+    
+    while left <= right:
+        mid = ( left + right ) // 2
+        if arr[mid] == target and arr[mid+1] > target:
+            return mid
+        elif arr[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
 
 def find_first_and_last(arr, target):
+    if len(arr) == 0 or arr[0] > target or arr[-1] < target:
+        print(f'{target} not found.\n')
+
     start = find_start(arr, target)
     end = find_end(arr, target)
+
     print(f'It starts in the index {start} and ends in the index {end}\n')
 
 if __name__ == '__main__':
